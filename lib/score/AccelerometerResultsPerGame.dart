2423848508ddea242ffc0ae3b153547a095075c0
4geniu <taketubo0714@gmail.com>
2023-12-18 10:20:55 +0900

    Definition of score calculation method

--- lib/score/AccelerometerResultsPerGame.dart
+++ lib/score/AccelerometerResultsPerGame.dart
@@ -1,6 +1,42 @@
 
 
-class AccelerometerResultsPerGame{
+class AccelerometerResultsPerGame extends Iterable<ThreeDPoint>{
+
+  List<ThreeDPoint> _accelerometerResultsPerGame = [];
 
   AccelerometerResultsPerGame(){;}
+  AccelerometerResultsPerGame.init(List<ThreeDPoint> accelerometerResultsPerGame){
+    _accelerometerResultsPerGame = accelerometerResultsPerGame;
+  }
+
+  void add(ThreeDPoint threeDPoint){
+    _accelerometerResultsPerGame.add(threeDPoint);
+  }
+
+  void clear(){
+    _accelerometerResultsPerGame.clear();
+  }
+
+  //iterator
+  Iterator<ThreeDPoint> get iterator => _accelerometerResultsPerGame.iterator;
+
+  //getters
+  int get length => _accelerometerResultsPerGame.length;
+  ThreeDPoint get first => _accelerometerResultsPerGame.first;
+  ThreeDPoint get last => _accelerometerResultsPerGame.last;
+  ThreeDPoint get single => _accelerometerResultsPerGame.single;
+  ThreeDPoint elementAt(int index) => _accelerometerResultsPerGame.elementAt(index);
+  bool get isEmpty => _accelerometerResultsPerGame.isEmpty;
+  bool get isNotEmpty => _accelerometerResultsPerGame.isNotEmpty;
+
+
+}
+
+class ThreeDPoint{
+  final double x;
+  final double y;
+  final double z;
+
+  ThreeDPoint(this.x, this.y, this.z);
+  ThreeDPoint.def(): x = 0, y = 0, z = 0;
 }
--- lib/score/GameScore.dart
+++ lib/score/GameScore.dart
@@ -1,11 +1,21 @@
 import './AccelerometerResultsPerGame.dart';
+import 'dart:math';
 
 class GameScore{
-  int _score = 5;
+  int _score = 0;
 
   GameScore(){
   }
   GameScore.def(AccelerometerResultsPerGame accelerometerResultsPerGame){
-    
+    for(var i = 0; i < accelerometerResultsPerGame.length-1; i++){
+      var deltaX = accelerometerResultsPerGame.elementAt(i+1).x - accelerometerResultsPerGame.elementAt(i).x;
+      var deltaY = accelerometerResultsPerGame.elementAt(i+1).y - accelerometerResultsPerGame.elementAt(i).y;
+      var deltaZ = accelerometerResultsPerGame.elementAt(i+1).z - accelerometerResultsPerGame.elementAt(i).z;
+
+
+      _score = sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ).toInt();
+    }
   }
+
+  int get score => _score;
 }
