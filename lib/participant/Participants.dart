570b47d141ad6bdee15c9323236d8417d90d6fc5
4geniu <taketubo0714@gmail.com>
2023-12-19 22:28:06 +0900

    feat: Iterator was implemented.

--- lib/participant/Participants.dart
+++ lib/participant/Participants.dart
@@ -1,14 +1,23 @@
 import 'Participant.dart';
 
 
-class Participants{
+class Participants extends Iterable<Participant>{
   late List<Participant> participants;
 
   Participants(int numberOfParticipants) {
-    participants =
-    List<Participant>.filled(numberOfParticipants, Participant());
-    for (int i = 0; i < numberOfParticipants; i++) {
-      participants[i] = Participant();
-    }
+    participants = List<Participant>.filled(numberOfParticipants, Participant());
   }
+
+  //iterator
+  Iterator<Participant> get iterator => participants.iterator;
+
+  //getters
+  int get length => participants.length;
+  Participant get first => participants.first;
+  Participant get last => participants.last;
+  Participant get single => participants.single;
+  Participant elementAt(int index) => participants.elementAt(index);
+  bool get isEmpty => participants.isEmpty;
+  bool get isNotEmpty => participants.isNotEmpty;
+
 }
